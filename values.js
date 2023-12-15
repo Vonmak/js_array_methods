c055f4e4e838ad0602c9c8ce71f6907368b77e62
Guest <von.makori@gmail.com>
2023-12-15 23:20:01 +0300

    explained the values method and gave an example.

---
+++ values.js
@@ -0,0 +1,10 @@
+// The values() method returns an Iterator that provides values for each index in the array.
+// It takes no arguments.
+
+// Example
+const arr= ['kevin', 'victor', 'jane'];
+const iterator = arr.values();
+
+for(const value of iterator){
+    console.log(value);
+}
