f2e98129c6cb0d71711b0dffd91e39aa90db918f
Guest <von.makori@gmail.com>
2023-12-15 23:25:49 +0300

    explained the at method and gave an example.

---
+++ at.js
@@ -0,0 +1,6 @@
+// The at() method returns element of specified index.
+// It takes argument: index
+
+// Example
+const arr =['apple', 'banana', 'oranges'];
+console.log(arr.at(1)); //output: banana
