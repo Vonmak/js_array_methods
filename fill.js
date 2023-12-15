e824fe034c92eba7ac8c02d0d366e44bf91fd7a8
Guest <von.makori@gmail.com>
2023-12-15 23:33:12 +0300

    explained the fill method and gave an example.

---
+++ fill.js
@@ -0,0 +1,10 @@
+// the fill() method fills all the elements of an array from start index to an end index with static values.
+// It can take upto three arguments;
+// 1. value to fill with,
+// 2. start index, and
+// 3. end index.
+
+// Example:
+const arr=['gray', 'yellow', 'orange', 'pink'];
+arr.fill('green',2,3);
+console.log(arr); // Outpu: ['gray', 'yellow', 'green', 'pink']
