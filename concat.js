192754eb49efab3a6785226317ba13cf67657ec0
Guest <von.makori@gmail.com>
2023-12-18 12:48:08 +0300

    explain the concat method with examples.

---
+++ concat.js
@@ -0,0 +1,10 @@
+// The concat() method is used to merge two or more arrays.
+// This method does not change the existing arrays, but instead returns a new array.
+
+// Example:
+let courses = ["Mobile", "programming", "basic electronics"];
+let more_courses = ["business", "arts", "community health"];
+
+let allCourses = courses.concat(more_courses);
+
+console.log(allCourses); //Output: [ 'Mobile','programming','basic electronics','business','arts','community health' ]
