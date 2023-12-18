dd05226d73bba9ad603e8925284d23b647845803
Guest <von.makori@gmail.com>
2023-12-18 12:54:52 +0300

    explain the reverse method with examples.

---
+++ reverse.js
@@ -0,0 +1,10 @@
+// The reverse() method reverses the order of the elements in the array.
+
+// Example:
+const arr=[1,2,3,4,5];
+arr.reverse();
+console.log(arr); //[ 5, 4, 3, 2, 1 ]
+
+const arr2= ['cow','cat', 'dog','hen'];
+arr2.reverse();
+console.log(arr2); //[ 'hen', 'dog', 'cat', 'cow' ]
