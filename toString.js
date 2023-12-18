845d15dc24d0818fe3c7a1446923492601b35758
Guest <von.makori@gmail.com>
2023-12-18 13:05:52 +0300

    explain the toString method with examples.

---
+++ toString.js
@@ -0,0 +1,6 @@
+// The toString method returns a string representing the array and its elements.
+
+// Example:
+const students=['james','john','kevin','mary'];
+const stud=students.toString();
+console.log(stud); //'james,john,kevin,mary'
