2ed3d64d8306a4539fcc27bff29d31a1b3fc4c26
Guest <von.makori@gmail.com>
2023-12-18 13:00:40 +0300

    explain the sort method with examples.

---
+++ sort.js
@@ -0,0 +1,7 @@
+// The sort() method sorts the elements of an array in place and returns the sorted array.
+// It can take an optional compare function as an argument.
+
+// Example:
+const arr= ['cow','hen','cat', 'dog'];
+arr.sort();
+console.log(arr); //[ 'cat', 'cow', 'dog', 'hen' ]
